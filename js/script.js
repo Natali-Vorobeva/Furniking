7db5faa2fb685b23de5ca90db5ba5f55f643fde6
Natali-Vorobeva <vorobjeva.natalia76@yandex.ru>
2022-08-07 00:20:06 +0300

    burger

--- js/script.js
+++ js/script.js
@@ -11,8 +11,21 @@
 // function changeImage('main__switch-block') {
 // 	form.pic.src = form.imagename.value;
 //  }
-function changeImage() {
-    var temp = document.getElementById("pic1").src;
-    document.getElementById("pic1").src = document.getElementById("pic2").src;
-    document.getElementById("pic2").src = temp;
-}
+// function changeImage() {
+// 	var temp = document.getElementById("pic1").src;
+// 	document.getElementById("pic1").src = document.getElementById("pic2").src;
+// 	document.getElementById("pic2").src = temp;
+// }
+
+// $(document).ready(function() {
+// 	$('.homeNav__burger-4').click(function(event) {
+// 		$('homeNav__burger-4,.homeNav__menu').toggleClass('active');
+// 		$('body').toggleClass('lock');
+// 	});
+// });
+$(document).ready(function () {
+	$('.homeNav__burger-4').click(function (event) {
+		$('.homeNav__burger-4,.homeNav__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
